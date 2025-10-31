0f43220886f52545c3ce23fcb214251532272895
mdafftfa <daffa11futn@gmail.com>
2025-11-01 01:42:22 +0700

    + add pagination at CartPortal.tsx + add pagination at Cart.tsx + update Dialog UI/UX

--- frontend/src/components/layouts/Cart.tsx
+++ frontend/src/components/layouts/Cart.tsx
@@ -1,24 +1,70 @@
 import {Component} from "react";
 import {
-    Box,
-    Button,
+    Box, Button,
+    ButtonGroup,
     Card,
     Dialog,
     Flex,
     Grid,
-    Heading,
-    Link,
+    Heading, IconButton, Link, Pagination,
     Separator,
     Text
 } from "@chakra-ui/react";
-import {PiTrolleyFill} from "react-icons/pi";
 import CartPortal from "@/components/modals/CartPortal.tsx";
+import {LuChevronLeft, LuChevronRight} from "react-icons/lu";
+import {PiTrolleyFill} from "react-icons/pi";
 
-export default class Cart extends Component {
+interface ProductItem {
+    id: number;
+    name: string;
+    price: number;
+    amount: number;
+    description: string
+}
+
+interface ProductState {
+    products: ProductItem[];
+    currentPage: number;
+    pageSize: number;
+    selectedProduct?: ProductItem;
+}
+
+export default class Cart extends Component<{}, ProductState> {
+
+    constructor(props: {}) {
+        super(props);
+        this.state = {
+            products: [],
+            currentPage: 1,
+            pageSize: 6,
+            selectedProduct: undefined,
+        };
+    }
+
+    setPage = (page: number) => {
+        this.setState({ currentPage: page });
+    };
+
+    selectProduct = (product: ProductItem) => {
+        this.setState({ selectedProduct: product });
+    };
+
+    addToCart = (product: ProductItem) => {
+        this.setState(prev => ({
+            products: [...prev.products, product]
+        }));
+    }
 
     render() {
+
+        const { products, currentPage, pageSize } = this.state;
+        const totalPages = Math.ceil(products.length / pageSize);
+        const start = (currentPage - 1) * pageSize;
+        const currentProducts = products.slice(start, start + pageSize);
+        const totalPrice = currentProducts.map((product) => product.price);
+
         return (
-            <Card.Root layerStyle={""} width="full" height={"3xl"}>
+            <Card.Root layerStyle={"fill.surface"} width="full" height={"3xl"}>
                 <Card.Header>
                     <Heading fontSize={"2xl"} fontWeight={"semibold"}>Order Details</Heading>
                     <Separator />
@@ -26,34 +72,68 @@ export default class Cart extends Component {
                 <Card.Body>
                     <Dialog.Root>
                         <Dialog.Trigger asChild>
-                            <Link display="block" rounded="sm" p="2.5" layerStyle="fill.surface">
-                                <Flex alignItems="center" justify="space-between">
-                                    <Flex alignItems="center" gap="2.5">
-                                        <Box layerStyle="fill.muted" fontSize="md" fontWeight="semibold" rounded="sm" px="5" py="2.5">
-                                            2
-                                        </Box>
-                                        <Grid>
-                                            <Text fontSize="md" fontWeight="semibold">Sate Ayam</Text>
-                                            <Text fontSize="sm">Rp. 5,000</Text>
-                                        </Grid>
-                                    </Flex>
-                                    <Text fontSize="md" fontWeight="bold">Rp. 15,000</Text>
-                                </Flex>
-                            </Link>
+                            <Grid>
+                                {products.length < 1 && (
+                                    <Text>No orders available</Text>
+                                )}
+                                {currentProducts.map((product) => {
+                                    return (
+                                        <Link display="block" rounded="sm" p="2.5" layerStyle={"fill.surface"} onClick={() => this.selectProduct(product)} >
+                                            <Flex alignItems="center" justify="space-between">
+                                                <Flex alignItems="center" gap="2.5">
+                                                    <Box layerStyle="fill.muted" fontSize="md" fontWeight="semibold" rounded="sm" px="2.5" py="1">
+                                                        {product.amount}
+                                                    </Box>
+                                                    <Grid>
+                                                        <Text fontSize="md" fontWeight="semibold">{product.name}</Text>
+                                                        <Text fontSize="sm">{product.price.toLocaleString()} IDR</Text>
+                                                    </Grid>
+                                                </Flex>
+                                                <Text fontSize="md" fontWeight="bold">{totalPrice} IDR</Text>
+                                            </Flex>
+                                        </Link>
+                                    );
+                                })}
+                            </Grid>
                         </Dialog.Trigger>
-                        <CartPortal />
+                        <CartPortal product={this.state.selectedProduct} />
                     </Dialog.Root>
-
 
                 </Card.Body>
                 <Card.Footer display={"grid"} gap={"5"}>
+                    <Separator />
+                    {products.length > 6 && (
+                        <Pagination.Root count={totalPages} pageSize={1}>
+                            <ButtonGroup variant="ghost" size="sm">
+                                <Pagination.PrevTrigger asChild>
+                                    <IconButton onClick={() => this.setPage(Math.max(1, currentPage - 1))}>
+                                        <LuChevronLeft />
+                                    </IconButton>
+                                </Pagination.PrevTrigger>
+
+                                {Array.from({ length: totalPages }).map((_, i) => (
+                                    <IconButton key={i} variant={i + 1 === currentPage ? "solid" : "outline"} onClick={() => this.setPage(i + 1)}>
+                                        {i + 1}
+                                    </IconButton>
+                                ))}
+
+                                <Pagination.NextTrigger asChild>
+                                    <IconButton onClick={() => this.setPage(Math.min(totalPages, currentPage + 1))}>
+                                        <LuChevronRight />
+                                    </IconButton>
+                                </Pagination.NextTrigger>
+                            </ButtonGroup>
+                        </Pagination.Root>
+                    )}
                     <Flex justify={"space-between"}>
                         <Text fontWeight={"light"} fontSize={"2xl"}>Total Price:</Text>
-                        <Text fontWeight={"extrabold"} fontSize={"2xl"}>Rp. 50,000</Text>
+                        <Text fontWeight={"extrabold"} fontSize={"2xl"}>
+                            {products.length === 0 ? "0 IDR" :   `${totalPrice} IDR`}
+                        </Text>
                     </Flex>
                     <Button variant={"solid"} backgroundColor={"teal.500"} size={"2xl"} fontSize={"2xl"}>
                         <PiTrolleyFill />
-                        BUY
+                        PAY
                     </Button>
                 </Card.Footer>
             </Card.Root>
--- frontend/src/components/modals/CartPortal.tsx
+++ frontend/src/components/modals/CartPortal.tsx
@@ -2,10 +2,10 @@ import {Component} from "react";
 import {
     Button,
     CloseButton,
-    Dialog, DialogCloseTrigger,
+    Dialog,
     Field,
     Flex,
-    Grid, Input, NumberInput,
+    HStack, IconButton, NumberInput,
     Portal,
     Separator,
     Stack,
@@ -13,21 +13,42 @@ import {
     Textarea
 } from "@chakra-ui/react";
 import {BiSave, BiTrash} from "react-icons/bi";
+import {LuMinus, LuPlus} from "react-icons/lu";
 
-export default class CartPortal extends Component {
-
-    constructor(props: any) {
-        super(props);
+interface CartPortalProps {
+    product?: {
+        id: number,
+        name: string,
+        price: number,
+        amount: number,
+        description: string,
     }
+}
+
+export default class CartPortal extends Component<CartPortalProps> {
+
+    proccessSave = () => {
+
+    };
+
+    processDelete = () => {
+
+    };
+
 
     render() {
+        const { product } = this.props;
+        if (!product) return null;
+
+        const totalPrice = (product.price * product.amount).toLocaleString();
+
         return (
             <Portal>
                 <Dialog.Backdrop />
                 <Dialog.Positioner>
                     <Dialog.Content layerStyle={"fill.surface"}>
                         <Dialog.Header>
-                            <Dialog.Title fontSize={"2xl"}>Sate Ayam</Dialog.Title>
+                            <Dialog.Title fontSize={"2xl"}>{product.name}</Dialog.Title>
                             <Separator />
                         </Dialog.Header>
                         <Dialog.CloseTrigger asChild>
@@ -37,29 +58,41 @@ export default class CartPortal extends Component {
                             <Stack gap="5">
                                 <Flex justify={"space-between"} alignItems={"center"}>
                                     <Text fontWeight={"medium"}>Total Price:</Text>
-                                    <Text fontWeight={"semibold"}>Rp. 15,000</Text>
+                                    <Text fontWeight={"semibold"}>{totalPrice} IDR</Text>
                                 </Flex>
                                 <Flex justify={"space-between"} alignItems={"center"}>
                                     <Text fontWeight={"medium"}>Amount:</Text>
-                                    <NumberInput.Root fontSize={"lg"} fontWeight={"semibold"} layerStyle={"fill.subtle"} defaultValue="10" width="100px">
-                                        <NumberInput.Control />
-                                        <NumberInput.Input />
+                                    <NumberInput.Root min={1} defaultValue={product.amount.toString()} unstyled spinOnPress={false} fontSize={"lg"} fontWeight={"semibold"} layerStyle={"fill.subtle"}>
+                                        <HStack gap="2">
+                                            <NumberInput.DecrementTrigger asChild>
+                                                <IconButton variant="outline" size="sm">
+                                                    <LuMinus />
+                                                </IconButton>
+                                            </NumberInput.DecrementTrigger>
+                                            <NumberInput.ValueText textAlign="center" fontSize="lg" minW="3ch" />
+                                            <NumberInput.IncrementTrigger asChild>
+                                                <IconButton variant="outline" size="sm">
+                                                    <LuPlus />
+                                                </IconButton>
+                                            </NumberInput.IncrementTrigger>
+                                        </HStack>
                                     </NumberInput.Root>
+
                                 </Flex>
                                 <Field.Root display={"grid"} gap={"5"} required>
                                     <Field.Label fontSize={"lg"}>
                                         Description: <Field.RequiredIndicator />
                                     </Field.Label>
-                                    <Textarea size={"xl"} placeholder="Example: Spicy, Half Rice" layerStyle={"fill.subtle"} />
+                                    <Textarea size={"xl"} defaultValue={product.description} placeholder="Example: Spicy, Half Rice" layerStyle={"fill.subtle"} />
                                 </Field.Root>
                             </Stack>
                         </Dialog.Body>
                         <Dialog.Footer>
-                            <Button variant={"solid"}>
+                            <Button variant={"solid"} onClick={this.proccessSave}>
                                 <BiSave />
                                 Save
                             </Button>
-                            <Button variant={"surface"}>
+                            <Button variant={"surface"} onClick={this.processDelete}>
                                 <BiTrash />
                                 Delete
                             </Button>
